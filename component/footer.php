ee8971da24524486c414dafb8db7d5fb9bc22cf5
Himm <ibrahimaryan46@gmail.com>
2024-10-31 23:23:55 +0700

    Membuat folder khusus untuk sidebar, header, footer dan membuat template page

---
+++ component/footer.php
@@ -0,0 +1,14 @@
+<!-- /komponen/footer.php -->
+<footer class="flex justify-between items-center mt-12 ml-64 p-8"> <!-- Tambahkan ml-64 untuk memberikan margin-left -->
+    <div class="flex items-center">
+        <img src="image/polines_logo.png" alt="Polines Logo" class="h-20 mr-2"> <!-- Logo Polines -->
+        <span class="font-bold text-4xl">Polivent</span>
+    </div>
+    <div class="text-right">
+        <p>Jl. Prof. Soedarto, Tembalang,</p>
+        <p>Kec. Tembalang, Kota Semarang,</p>
+        <p>Jawa Tengah 50275</p>
+        <p>Telp (024) 7473417</p>
+        <p>Email: polines@ac.id</p>
+    </div>
+</footer>
